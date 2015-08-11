2481abdb2a1ecfc62e9d5c2459e3640df28e5462
amal-roshan <scaryamal@gmail.com>
2015-08-11 19:41:57 +0530

    Create CircleShape.cs

---
+++ CircleShape.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Drawing.Drawing2D;
+namespace Paint3
+{
+    class CircleShape:Shape
+    {
+        Point firstLocation;
+        Point lastLocation;
+        Pen myPen;
+        public Point FirstLocation
+        {
+            get
+            {
+                return firstLocation;
+            }
+            set
+            {
+                firstLocation = value;
+            }
+        }
+
+        public Point LastLocation
+        {
+            get
+            {
+                return lastLocation;
+            }
+            set
+            {
+                lastLocation = value;
+            }
+        }
+        public void GetPen(Pen pen)
+        {
+            myPen = pen;
+        }
+        public override PictureBox DrawShape(PictureBox pic, Point point)
+        {
+            System.Drawing.Rectangle rect = new System.Drawing.Rectangle(Math.Min(point.X, firstLocation.X), Math.Min(point.Y, firstLocation.Y), Math.Abs(point.X - firstLocation.X), Math.Abs(point.Y - firstLocation.Y));
+            Graphics g = Graphics.FromImage(pic.Image);
+            //g.DrawEllipse(myPen, firstLocation.X, firstLocation.Y, Math.Abs(point.X - firstLocation.X), Math.Abs(point.Y - firstLocation.Y));
+            g = pic.CreateGraphics();
+            g.DrawEllipse(myPen, rect);
+            pic.Image = pic.Image;
+            return pic;
+        }    
+    }
+}
